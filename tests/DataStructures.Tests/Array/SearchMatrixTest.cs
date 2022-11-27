d2befb86a6f70824128610c37e3e40343b70baae
aguiartomas <aguiar.tomashb+github@gmail.com>
2022-11-27 13:17:31 -0300

    added search matrix

---
+++ tests/DataStructures.Tests/Array/SearchMatrixTest.cs
@@ -0,0 +1,55 @@
+using DataStructures.Array;
+using Xunit;
+
+namespace DataStructures.Tests.Array;
+
+public class SearchMatrixTest
+{
+    [Theory]
+    [InlineData(true, 3, 
+        new[] { 1, 3, 5, 7 }, 
+        new[] { 10, 11, 16, 20 }, 
+        new[] { 23, 30, 34, 60 })]
+    [InlineData(false, 13, 
+        new[] { 1, 3, 5, 7 }, 
+        new[] { 10, 11, 16, 20 }, 
+        new[] { 23, 30, 34, 60 })]
+    [InlineData(false, 0, new[] { 1 })]
+    [InlineData(true, 3, new[] { 1 }, new[] { 3 })]
+    [InlineData(false, 0, new[] { 1, 1 })]
+    [InlineData(false, 2, new[] { 1 })]
+    [InlineData(false, 2, new[] { 1 }, new[] { 3 })]
+    [InlineData(false, 0, new[] { 1 }, new[] { 3 })]
+    [InlineData(true, 3, new[] { 1 }, new[] { 3 }, new[] { 5 })]
+    [InlineData(true, 3, new[] { 1, 3 })]
+    public void SearchBruteForce_Test(bool expected, int target, params int[][] matrix)
+    {
+        var result = SearchMatrix.SearchBruteForce(matrix, target);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Theory]
+    [InlineData(true, 3, 
+        new[] { 1, 3, 5, 7 }, 
+        new[] { 10, 11, 16, 20 }, 
+        new[] { 23, 30, 34, 60 })]
+    [InlineData(false, 13, 
+        new[] { 1, 3, 5, 7 }, 
+        new[] { 10, 11, 16, 20 }, 
+        new[] { 23, 30, 34, 60 })]
+    [InlineData(false, 0, new[] { 1 })]
+    [InlineData(true, 3, new[] { 1 }, new[] { 3 })]
+    [InlineData(false, 0, new[] { 1, 1 })]
+    [InlineData(false, 2, new[] { 1 })]
+    [InlineData(false, 2, new[] { 1 }, new[] { 3 })]
+    [InlineData(false, 0, new[] { 1 }, new[] { 3 })]
+    [InlineData(true, 3, new[] { 1 }, new[] { 3 }, new[] { 5 })]
+    [InlineData(true, 3, new[] { 1, 3 })]
+    public void BinarySearch_Test(bool expected, int target, params int[][] matrix)
+    {
+        var result = SearchMatrix.BinarySearch(matrix, target);
+        
+        Assert.Equal(expected, result);
+    }
+}
