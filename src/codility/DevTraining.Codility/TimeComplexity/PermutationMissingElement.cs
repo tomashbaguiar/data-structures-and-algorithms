1b21faa4ac8f2ddfe0f48d0440b1ea4910120f46
aguiartomas <aguiar.tomashb+github@gmail.com>
2022-11-30 20:07:50 -0300

    added permutation missing element

---
+++ src/codility/DevTraining.Codility/TimeComplexity/PermutationMissingElement.cs
@@ -0,0 +1,17 @@
+namespace DevTraining.Codility.TimeComplexity;
+
+public static class PermutationMissingElement
+{
+    public static int Solution(int[] array)
+    {
+        var ordArray = array.OrderBy(c => c).ToArray();
+        var count = 1;
+        foreach (var e in ordArray)
+        {
+            if (e != count) break;
+            count++;
+        }
+
+        return count;
+    }
+}
