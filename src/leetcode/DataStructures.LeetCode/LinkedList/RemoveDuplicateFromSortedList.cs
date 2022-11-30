68810b4415313961097daed8bc2fb799bb598bd7
Tomás Aguiar <aguiar.tomashb+github@gmail.com>
2022-11-30 13:33:38 -0300

    added remove duplicate in sorted linked list

---
+++ src/leetcode/DataStructures.LeetCode/LinkedList/RemoveDuplicateFromSortedList.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructures.LeetCode.LinkedList;
+
+public static class RemoveDuplicateFromSortedList
+{
+    public static ListNode? DeleteIterative(ListNode? head)
+    {
+        if (head == null) return null;
+        
+        var curr = head.next;
+        var last = head;
+        while (curr != null)
+        {
+            if (curr.val != last.val)
+            {
+                last = curr;
+                curr = curr.next;
+                continue;
+            }
+
+            last.next = curr.next;
+            curr = curr.next;
+        }
+
+        return head;
+    }
+
+    public static ListNode? DeleteRecursive(ListNode? head)
+    {
+        if(head?.next == null) return head;
+        
+        head.next = DeleteRecursive(head.next);
+        return head.next != null && head.val == head.next.val ? head.next : head;
+    }
+}
