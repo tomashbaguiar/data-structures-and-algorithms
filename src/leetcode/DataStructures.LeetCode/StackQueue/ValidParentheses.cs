318fd2a93def1247ca6fcb589c46b57b380221d5
Tomás Aguiar <aguiar.tomashb+github@gmail.com>
2022-12-01 15:21:13 -0300

    added valid parentheses

---
+++ src/leetcode/DataStructures.LeetCode/StackQueue/ValidParentheses.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructures.LeetCode.StackQueue;
+
+public static class ValidParentheses
+{
+    private static readonly Dictionary<char, char> Lookup = new()
+    {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+    };
+
+    public static bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if (Lookup.ContainsKey(c))
+            {
+                stack.Push(c);
+                continue;
+            }
+
+            if (!stack.Any() || Lookup[stack.Peek()] != c) return false;
+
+            stack.Pop();
+        }
+
+        return !stack.Any();
+    }
+}
