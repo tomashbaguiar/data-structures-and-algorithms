0a996871ba15063b77cf6f8ea302e48b89f4b362
Tomás Aguiar <aguiar.tomashb+github@gmail.com>
2022-12-01 16:07:40 -0300

    added implement queue using stacks

---
+++ src/leetcode/DataStructures.LeetCode/StackQueue/MyQueue.cs
@@ -0,0 +1,35 @@
+﻿namespace DataStructures.LeetCode.StackQueue;
+
+public class MyQueue
+{
+    private readonly Stack<int> _mainStack = new();
+    private readonly Stack<int> _secondStack = new();
+    
+    public void Push(int item)
+    {
+        _mainStack.Push(item);
+    }
+
+    public int Peek()
+    {
+        while (_mainStack.Count != 0) _secondStack.Push(_mainStack.Pop());
+        var item = _secondStack.Peek();
+        while (_secondStack.Count != 0) _mainStack.Push(_secondStack.Pop());
+
+        return item;
+    }
+
+    public int Pop()
+    {
+        while (_mainStack.Count != 0) _secondStack.Push(_mainStack.Pop());
+        var item = _secondStack.Pop();
+        while (_secondStack.Count != 0) _mainStack.Push(_secondStack.Pop());
+
+        return item;
+    }
+
+    public bool Empty()
+    {
+        return _mainStack.Count == 0;
+    }
+}
---
+++ tests/leetcode/DataStructures.LeetCode.Tests/StackQueue/QueueImplementedWithStacksTest.cs
@@ -0,0 +1,23 @@
+﻿using DataStructures.LeetCode.StackQueue;
+using Xunit;
+
+namespace DataStructures.LeetCode.Tests.StackQueue;
+
+public class QueueImplementedWithStacksTest
+{
+    [Fact]
+    public void FirstTest()
+    {
+        var queue = new MyQueue();
+        queue.Push(1);
+        queue.Push(2);
+
+        var first = queue.Peek();
+        var second = queue.Pop();
+        var third = queue.Empty();
+        
+        Assert.Equal(1, first);
+        Assert.Equal(1, second);
+        Assert.False(third);
+    }
+}
