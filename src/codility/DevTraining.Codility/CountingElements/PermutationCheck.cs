a3802194305d8cec155d7ca040c97de1cdc5b696
Tomás Aguiar <aguiar.tomashb+github@gmail.com>
2022-12-01 21:13:18 -0300

    added permutation check

---
+++ src/codility/DevTraining.Codility/CountingElements/PermutationCheck.cs
@@ -0,0 +1,16 @@
+﻿namespace DevTraining.Codility.CountingElements;
+
+public static class PermutationCheck
+{
+    public static int Solution(int[] array)
+    {
+        var hash = new HashSet<int>();
+        foreach (var e in array)
+        {
+            if (hash.Contains(e)) return 0;
+            hash.Add(e);
+        }
+
+        return hash.Max() == array.Length ? 1 : 0;
+    }
+}
