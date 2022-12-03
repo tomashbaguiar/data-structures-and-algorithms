250c0ee77e9682ee7a443256289c46a6d7891446
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-02 22:06:04 -0300

    added max counter first solution

---
+++ src/codility/DevTraining.Codility/CountingElements/MaxCounters.cs
@@ -0,0 +1,29 @@
+namespace DevTraining.Codility.CountingElements;
+
+public static class MaxCounters
+{
+    // O(nm)
+    public static int[] NotEfficientSolution(int n, int[] array)
+    {
+        var counters = new int[n];
+        var max = 0;
+        foreach (var e in array)
+        {
+            if (e >= 1 && e <= n)
+            {
+                var c = ++counters[e - 1];
+                max = c > max ? c : max;
+                continue;
+            }
+            
+            if (e != n + 1) continue;
+
+            for (var j = 0; j < n; j++)
+            {
+                counters[j] = max;
+            }
+        }
+
+        return counters;
+    }
+}
