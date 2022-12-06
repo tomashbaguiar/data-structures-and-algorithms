f7453e41a5e3ab00d0511e21e5bb781d6899d239
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-06 09:41:48 -0300

    to merge commit

---
+++ src/codility/DevTraining.Codility/CountingElements/MaxCounters.cs
@@ -0,0 +1,27 @@
+﻿namespace DevTraining.Codility.CountingElements;
+
+public static class MaxCounters
+{
+    public static int[] Solution(int[] array, int n)
+    {
+        var counters = new int[n];
+        var max = 0;
+        foreach (var e in array)
+        {
+            if (e >= 1 && e <= n)
+            {
+                var c = ++counters[e - 1];
+                max = c > max ? c : max;
+                continue;
+            }
+
+            if (e != n + 1) continue;
+            for (var j = 0; j < n; j++)
+            {
+                counters[j] = max;
+            }
+        }
+
+        return counters;
+    }
+}
---
+++ tests/codility/DevTraining.Codility.Tests/CountingElements/MaxCountersTest.cs
@@ -0,0 +1,16 @@
+﻿using DevTraining.Codility.CountingElements;
+using Xunit;
+
+namespace DevTraining.Codility.Tests.CountingElements;
+
+public class MaxCountersTest
+{
+    [Theory]
+    [InlineData(new[] { 3, 4, 4, 6, 1, 4, 4 }, 5, new[] { 3, 2, 2, 4, 2 })]
+    public void Solution_Test(int[] array, int n, int[] expected)
+    {
+        var result = MaxCounters.Solution(array, n);
+        
+        Assert.Equal(expected, result);
+    }
+}
