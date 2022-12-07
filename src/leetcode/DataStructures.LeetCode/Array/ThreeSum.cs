acd28810d7db55d334c72f14b82e4fe230165b0c
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-07 14:00:43 -0300

    added three sum

--- src/leetcode/DataStructures.LeetCode/Array/ThreeSum.cs
+++ src/leetcode/DataStructures.LeetCode/Array/ThreeSum.cs
@@ -1,6 +1,45 @@
 ﻿namespace DataStructures.LeetCode.Array;
 
-public class ThreeSum
+public static class ThreeSum
 {
-    
+    public static IList<IList<int>> Solution(int[] nums)
+    {
+        var sol = new List<IList<int>>();
+        if (nums.Length == 0) return sol;
+
+        var hash  = new HashSet<(int, int, int)>();
+        var array = nums.OrderBy(c => c)
+            .ToArray(); 
+        for (var i = 0; i < array.Length - 2; i++)
+        {
+            var j = i + 1;
+            var k = array.Length - 1;
+            var x = array[i];
+            while (j < k)
+            {
+                var (y, z) = (array[j], array[k]);
+                var sum = x + y + z;
+                if (sum == 0)
+                {
+                    var c = (array[i], array[j++], array[k--]);
+                    if (!hash.Contains(c))
+                    {
+                        hash.Add(c);
+                        sol.Add(new List<int> { x, y, z });
+                    }
+                    continue;
+                }
+
+                if (sum > 0)
+                {
+                    k--;
+                    continue;
+                }
+
+                j++;
+            }
+        }
+        
+        return sol;
+    }
 }
--- tests/leetcode/DataStructures.LeetCode.Tests/Array/ThreeSumTest.cs
+++ tests/leetcode/DataStructures.LeetCode.Tests/Array/ThreeSumTest.cs
@@ -1,6 +1,20 @@
-﻿namespace DataStructures.LeetCode.Tests.Array;
+﻿using System.Linq;
+using DataStructures.LeetCode.Array;
+using Xunit;
+
+namespace DataStructures.LeetCode.Tests.Array;
 
 public class ThreeSumTest
 {
-    
+    [Theory]
+    [InlineData(new[] { -1, 0, 1, 2, -1, -4 }, new[] { -1, -1, 2 }, new[] { -1, 0, 1 })]
+    [InlineData(new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0 })]
+    public void Solution_Test(int[] array, params int[][] expected)
+    {
+        var result = ThreeSum.Solution(array)
+            .Select(l => l.ToArray())
+            .ToArray();
+        
+        Assert.Equal(expected, result);
+    }
 }
