eadb2953b160a46c3ccd2e955506ad5dd062c67b
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-07 11:22:31 -0300

    added single element

---
+++ src/leetcode/DataStructures.LeetCode/Array/SingleElement.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructures.LeetCode.Array;
+
+public static class SingleElement
+{
+    public static int Find(int[] array)
+    {
+        if (array.Length == 0) return 0;
+        
+        var hash = new Dictionary<int, int>();
+        foreach (var e in array)
+        {
+            if (!hash.ContainsKey(e))
+            {
+                hash.Add(e, 1);
+                continue;
+            }
+            
+            hash[e]++;
+        }
+
+        return hash.First(e => e.Value == 1).Key;
+    }
+}
---
+++ tests/leetcode/DataStructures.LeetCode.Tests/Array/SingleElementTest.cs
@@ -0,0 +1,18 @@
+﻿using DataStructures.LeetCode.Array;
+using Xunit;
+
+namespace DataStructures.LeetCode.Tests.Array;
+
+public class SingleElementTest
+{
+    [Theory]
+    [InlineData(new[] { 2, 2, 1 }, 1)]
+    [InlineData(new[] { 4, 1, 2, 1, 2 }, 4)]
+    [InlineData(new[] { 1 }, 1)]
+    public void Find_Test(int[] array, int expected)
+    {
+        var result = SingleElement.Find(array);
+        
+        Assert.Equal(expected, result);
+    }
+}
