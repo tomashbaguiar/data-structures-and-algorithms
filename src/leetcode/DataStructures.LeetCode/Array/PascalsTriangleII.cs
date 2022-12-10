652b2ed00763602fc7f6b829fb4e66c027423ef0
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-09 21:19:56 -0300

    added pascals triangle ii

---
+++ src/leetcode/DataStructures.LeetCode/Array/PascalsTriangleII.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructures.LeetCode.Array;
+
+public static class PascalsTriangleIi
+{
+    public static IList<int> GetRow(int rowIndex)
+    {
+        var triangle = PascalsTriangle.Generate(rowIndex + 1);
+        return triangle[rowIndex];
+    }
+}
