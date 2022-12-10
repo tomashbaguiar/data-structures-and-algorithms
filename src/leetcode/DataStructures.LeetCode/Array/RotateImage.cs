34edc720835901736ba406ca05fcda2576d08662
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-09 22:23:57 -0300

    added rotate matrix

---
+++ src/leetcode/DataStructures.LeetCode/Array/RotateImage.cs
@@ -0,0 +1,26 @@
+﻿namespace DataStructures.LeetCode.Array;
+
+public static class RotateImage
+{
+    public static void Rotate(int[][] matrix)
+    {
+        var n = matrix.Length;
+        if (n == 1) return;
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = i + 1; j < n; j++)
+            {
+                (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+            }
+        }
+        
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n / 2; j++)
+            {
+                (matrix[i][j], matrix[i][n - j - 1]) = (matrix[i][n - j - 1], matrix[i][j]);
+            }
+        }
+    }
+}
---
+++ tests/leetcode/DataStructures.LeetCode.Tests/Array/RotateImageTest.cs
@@ -0,0 +1,39 @@
+﻿using DataStructures.LeetCode.Array;
+using Xunit;
+
+namespace DataStructures.LeetCode.Tests.Array;
+
+public class RotateImageTest
+{
+    [Fact]
+    public void Rotate_Test()
+    {
+        var matrix = new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 } };
+        var expected = new[] { new[] { 7, 4, 1 }, new[] { 8, 5, 2 }, new[] { 9, 6, 3 } };
+
+        RotateImage.Rotate(matrix);
+        
+        Assert.Equal(expected, matrix);
+    }
+
+    [Fact]
+    public void Rotate_Test_Big()
+    {
+        var matrix = new[] { 
+            new[] { 5, 1, 9, 11 }, 
+            new[] { 2, 4, 8, 10 }, 
+            new[] { 13, 3, 6, 7 }, 
+            new[] { 15, 14, 12, 16 } 
+        };
+        var expected = new[] { 
+            new[] { 15, 13, 2, 5 }, 
+            new[] { 14, 3, 4, 1 }, 
+            new[] { 12, 6, 8, 9 }, 
+            new[] { 16, 7, 10, 11 } 
+        };
+        
+        RotateImage.Rotate(matrix);
+        
+        Assert.Equal(expected, matrix);
+    }
+}
