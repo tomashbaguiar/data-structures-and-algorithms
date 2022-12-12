1e9239005e97e95c95d12fc6d6e1ddfda6369d3b
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-12 17:31:45 -0300

    added adding strings

---
+++ src/leetcode/DataStructures.LeetCode/String/AddingStrings.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace DataStructures.LeetCode.String;
+
+public static class AddingStrings
+{
+    private const int Offset = '0';
+
+    public static string Add(string num1, string num2)
+    {
+        var n1Len = num1.Length - 1;
+        var n2Len = num2.Length - 1;
+        var digits = new Stack<char>();
+        var sum = 0;
+        while (n1Len >= 0 || n2Len >= 0 || sum > 0)
+        {
+            var n1 = n1Len >= 0 ? num1[n1Len--] - Offset : 0;
+            var n2 = n2Len >= 0 ? num2[n2Len--] - Offset : 0;
+            sum += n1 + n2;
+            if (sum < 10)
+            {
+                digits.Push((char)(sum + Offset));
+                sum = 0;
+                continue;
+            }
+
+            digits.Push((char)(sum - 10 + Offset));
+            sum = 1;
+        }
+
+        var sb = new StringBuilder();
+        while (digits.Count > 0) sb.Append(digits.Pop());
+
+        return sb.ToString();
+    }
+}
---
+++ tests/leetcode/DataStructures.LeetCode.Tests/String/AddingStringsTest.cs
@@ -0,0 +1,18 @@
+﻿using DataStructures.LeetCode.String;
+using Xunit;
+
+namespace DataStructures.LeetCode.Tests.String;
+
+public class AddingStringsTest
+{
+    [Theory]
+    [InlineData("11", "123", "134")]
+    [InlineData("1", "9", "10")]
+    [InlineData("6913259244", "71103343", "6984362587")]
+    public void Add_Test(string num1, string num2, string expected)
+    {
+        var result = AddingStrings.Add(num1, num2);
+        
+        Assert.Equal(expected, result);
+    }
+}
