3f7bfcc5800b5716b47be05660ed7ff7425752ac
Tomás Aguiar <tomas.aguiar@outlook.com>
2022-12-14 18:45:25 -0300

    wip: subarray sum equals k

---
+++ src/leetcode/DataStructures.LeetCode/Array/SubarraySumEqualsK.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructures.LeetCode.Array;
+
+public static class SubarraySumEqualsK
+{
+    public static int FindBruteForce(int[] array, int k)
+    {
+        var n = array.Length;
+        if (n == 0) return 0;
+        if (n == 1 && array[0] == k) return 1;
+
+        var allSum = array.Sum();
+        if (allSum < k) return 0;
+        if (allSum == k) return 1;
+
+        var count = 0;
+        for (var i = 0; i < n; i++)
+        {
+            var sum = array[i];
+            var j = i + 1;
+            while (j < n && sum != k)
+            {
+                sum += array[j++];
+            }
+
+            if (sum == k) count++;
+        }
+
+        return count;
+    }
+}
---
+++ tests/leetcode/DataStructures.LeetCode.Tests/Array/SubarraySumEqualsKTest.cs
@@ -0,0 +1,17 @@
+﻿using DataStructures.LeetCode.Array;
+using Xunit;
+
+namespace DataStructures.LeetCode.Tests.Array;
+
+public class SubarraySumEqualsKTest
+{
+    [Theory]
+    [InlineData(new[] { 1, 1, 1 }, 2, 2)]
+    [InlineData(new[] { 1, 2, 3 }, 3, 2)]
+    public void FindBruteForce_Test(int[] array, int k, int expected)
+    {
+        var result = SubarraySumEqualsK.FindBruteForce(array, k);
+        
+        Assert.Equal(expected, result);
+    }
+}
